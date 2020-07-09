06981485f9c189e859e875bfcefff65c23ce7e98
RodrigoRP <grohlbr@hotmail.com>
2020-07-09 20:59:01 -0300

    adding tests for PersonService

--- src/main/java/com/rodrigoramos/registration/model/Person.java
+++ src/main/java/com/rodrigoramos/registration/model/Person.java
@@ -1,9 +1,6 @@
 package com.rodrigoramos.registration.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
--- src/test/java/com/rodrigoramos/registration/service/PersonServiceTest.java
+++ src/test/java/com/rodrigoramos/registration/service/PersonServiceTest.java
@@ -2,6 +2,7 @@ package com.rodrigoramos.registration.service;
 
 import com.rodrigoramos.registration.model.Person;
 import com.rodrigoramos.registration.repository.PersonRepository;
+import com.rodrigoramos.registration.service.exception.ObjectNotFoundException;
 import com.rodrigoramos.registration.service.impl.PersonServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +12,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -37,6 +41,39 @@ class PersonServiceTest {
         //then
         Assertions.assertNotNull(savedPerson);
         then(personRepository).should().save(any(Person.class));
+    }
+
+    @Test
+    @DisplayName("Test findById Success")
+    void findPersonByIdTest() {
+        //given
+        Person person = Person.builder()
+                .id(1L)
+                .cpf("70804614059")
+                .email("adamastor@bol.com.br")
+                .fullName("Adamastor Silva")
+                .nationality("Brazilian")
+                .placeOfBirth("Florianópolis")
+                .gender("Masculino")
+                .build();
+        given(personRepository.findById(1L)).willReturn(Optional.of(person));
+
+        //when
+        Person savedPerson = personService.findById(1L);
+
+        //then
+        Assertions.assertNotNull(savedPerson);
+        Assertions.assertEquals(person, savedPerson);
+    }
+
+    @Test
+    @DisplayName("Test findById Not Found")
+    void findPersonByIdNotFound() {
+        Exception exception = assertThrows(
+                ObjectNotFoundException.class,
+                () -> personService.findById(22L));
+
+        Assertions.assertTrue(exception.getMessage().contains("not found"));
     }
 
 
